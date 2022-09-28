6457a8b80a36b24528e92f9f6005bc71a3d875c7
Vishal Ramani <Vishal.Ramani@ibm.com>
2022-09-28 20:27:38 +0530

    Initial first commit with all files from personal org repo

---
+++ main.tf
@@ -0,0 +1,69 @@
+locals {
+  name          = "ubi-helm"
+  bin_dir       = module.setup_clis.bin_dir
+  yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
+  service_url   = "http://${local.name}.${var.namespace}"
+  cluster_type = var.cluster_type == "kubernetes" ? "kubernetes" : "openshift"
+  values_content = {
+    ubi-helm= {
+      "replicaCount": 1
+      "image.repository" = "registry.access.redhat.com/ubi8/ubi"
+      "image.tag" = "latest"
+      "command" = "${var.command}"
+    }
+  }
+  layer = "applications"
+  type  = "base"
+  application_branch = "main"
+  namespace = var.namespace
+  layer_config = var.gitops_config[local.layer]
+}
+
+module setup_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+}
+
+resource null_resource create_yaml {
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.yaml_dir}'"
+
+    environment = {
+      VALUES_CONTENT = yamlencode(local.values_content)
+    }
+  }
+}
+
+resource null_resource setup_gitops {
+  depends_on = [null_resource.create_yaml]
+
+  triggers = {
+    name = local.name
+    namespace = var.namespace
+    yaml_dir = local.yaml_dir
+    server_name = var.server_name
+    layer = local.layer
+    type = local.type
+    git_credentials = yamlencode(var.git_credentials)
+    gitops_config   = yamlencode(var.gitops_config)
+    bin_dir = local.bin_dir
+  }
+
+  provisioner "local-exec" {
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+}
---
+++ scripts/create-yaml.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
+CHART_DIR=$(cd "${SCRIPT_DIR}/../chart/ubi-helm"; pwd -P)
+
+NAME="$1"
+DEST_DIR="$2"
+
+## Add logic here to put the yaml resource content in DEST_DIR
+mkdir -p "${DEST_DIR}"
+cp -R "${CHART_DIR}/"* "${DEST_DIR}"
+
+if [[ -n "${VALUES_CONTENT}" ]]; then
+  echo "${VALUES_CONTENT}" > "${DEST_DIR}/values.yaml"
+fi
+find "${DEST_DIR}" -name "*"
+echo "Files in output path"
+ls -l "${DEST_DIR}"
