46ddfdb3056e017bb12ac3bb38f235d269f2a678
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-09-30 16:09:27 +0200

    update stage3

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -3,12 +3,12 @@ resource local_file write_outputs {
   filename = "gitops-output.json"
 
   content = jsonencode({
-    name        = module.gitops_module.name
-    branch      = module.gitops_module.branch
-    namespace   = module.gitops_module.namespace
-    server_name = module.gitops_module.server_name
-    layer       = module.gitops_module.layer
-    layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
-    type        = module.gitops_module.type
+    name        = module.gitops_ubi.name
+    branch      = module.gitops_ubi.branch
+    namespace   = module.gitops_ubi.namespace
+    server_name = module.gitops_ubi.server_name
+    layer       = module.gitops_ubi.layer
+    layer_dir   = module.gitops_ubi.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_ubi.layer == "services" ? "2-services" : "3-applications")
+    type        = module.gitops_ubi.type
   })
 }
